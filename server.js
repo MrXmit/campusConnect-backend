5636dadfb341032c91dd0e28659ab16ed1775bfd
MrXmit <tonixsmit@gmail.com>
2023-10-12 15:18:27 +0300

    change file name

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ import './config/database.js'
 import { router as profilesRouter } from './routes/profiles.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as servicesRouter } from './routes/services.js'
+import { router as schoolsRouter } from './routes/schools.js'
 
 // create the express app
 const app = express()
@@ -26,6 +27,7 @@ app.use(formData.parse())
 app.use('/api/profiles', profilesRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/services', servicesRouter)
+app.use('/api/schools', schoolsRouter)
 
 // handle 404 errors
 app.use(function (req, res, next) {
